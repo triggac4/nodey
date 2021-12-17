74536fd23661ca901e533426648c2f1fe5aa1e8c
offokansi kenechukwu <koffokansi@gmail.com>
2021-12-17 04:14:46 +0100

    models created

--- database task/controller/task_controller.js
+++ database task/controller/task_controller.js
@@ -1,20 +1,31 @@
-function getAllTasks(req,res){
-    res.send('working well')
-}
-function createTask(req,res){
-    res.send('creates a task')
-}
-function getTask(req,res){
-    const{id}=req.params
-    res.send(`gets a task with id:${id} `)
-}
-function editTask(req,res){
-    const{id}=req.params
-    res.send(`edit a task with id:${id} `)
-}
-function deleteTask(req,res){
-    const{id}=req.params
-    res.send(`delete a task with id:${id} `)
+const TaskModel = require("../models/task");
+
+function getAllTasks(req, res) {
+  res.send("working well");
 }
 
-module.exports={getAllTasks,deleteTask,createTask,getTask,editTask}
+async function createTask(req, res) {
+  try {
+    const task = await TaskModel.create(req.body);
+    res.json(task);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function getTask(req, res) {
+  const { id } = req.params;
+  res.send(`gets a task with id:${id} `);
+}
+
+function editTask(req, res) {
+  const { id } = req.params;
+  res.send(`edit a task with id:${id} `);
+}
+
+function deleteTask(req, res) {
+  const { id } = req.params;
+  res.send(`delete a task with id:${id} `);
+}
+
+module.exports = { getAllTasks, deleteTask, createTask, getTask, editTask };
---
+++ database task/models/task.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const taskSchema=mongoose.Schema({name:String,completed:Boolean})
+
+module.exports= mongoose.model('Task',taskSchema)
