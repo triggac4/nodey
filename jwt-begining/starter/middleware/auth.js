f828e2e4ca370b86adca070621af055bba292c22
kene3055 <koffokansi@gmail.com>
2022-03-09 05:26:11 +0100

    console.logs removed

--- jwt-begining/starter/middleware/auth.js
+++ jwt-begining/starter/middleware/auth.js
@@ -2,15 +2,12 @@ const JWT = require("jsonwebtoken");
 const error = require("../errors");
 const authentication = async (req, res, next) => {
     const authHeader = req.headers.authorization;
-    console.log(authHeader);
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         throw new error.UnAuthorizeError("credentials missing");
     }
     const token = authHeader.split(" ")[1].trim();
     try {
-        console.log(token);
         decoded = JWT.verify(token, process.env.JWT_SECRET);
-        console.log(decoded);
         req.decoded = decoded;
         next();
     } catch (e) {
