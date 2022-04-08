ff3ecc2df4ad21feaa7da0ec0d69ad9b3ffabde3
kene3055 <koffokansi@gmail.com>
2022-04-08 09:25:20 +0100

    mongo errors caught

--- job api/starter/middleware/error-handler.js
+++ job api/starter/middleware/error-handler.js
@@ -2,10 +2,30 @@ const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err);
+    const customError = {
+        msg: err.message || "internal sever error",
+        status: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    };
     if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({ msg: err.message });
+        return res.status(customError.status).json({ msg: customError.msg });
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+    if (err.name) {
+        customError.status = StatusCodes.BAD_REQUEST;
+    }
+    if (err.name && err.name === "CastError") {
+        customError.status = StatusCodes.BAD_REQUEST;
+        customError.msg = "check tp see if yo";
+    }
+    if (err.name && err.name === "ValidationError") {
+        customError.status = StatusCodes.BAD_REQUEST;
+    }
+    if (err.code && err.code === 11000) {
+        customError.msg = `duplicate value error for ${Object.keys(
+            err.keyValue
+        )} `;
+        customError.status = StatusCodes.BAD_REQUEST;
+    }
+    return res.status(customError.status).json({ err: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
